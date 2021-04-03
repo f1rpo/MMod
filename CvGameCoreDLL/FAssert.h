2dadb3a6d73bcf7ef5a6bcefa53ca9349376414e
f1rpo <no.m@il.specified>
2021-04-03 14:19:52 +0200

    Some small updates to the build system

--- CvGameCoreDLL/FAssert.h
+++ CvGameCoreDLL/FAssert.h
@@ -14,7 +14,7 @@
 
 /*  f1rpo: Inlining functions with assertions could be a bad choice.
 	Let's at least not force-inline. */
-#define __forceinline inline
+#define __forceinline __inline
 
 #ifdef WIN32
 
